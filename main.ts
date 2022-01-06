6864b3eb7be1aaf7a5344fb9a51aa105da5f20bd
Coder002-239 <90915811+Coder002-239@users.noreply.github.com>
2022-01-06 10:18:49 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+tiles.setTilemap(tilemap`Test Tilemap1`)
+let mySprite = sprites.create(assets.image`Player1Stationary`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`Player2Stationary`, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
